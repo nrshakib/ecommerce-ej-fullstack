b37c9fcf23781ef83d8cf838b38a579ca6ffe430
NRS\nrsha <nrshakib007@gmail.com>
2023-08-04 23:19:24 +0600

    initialized server side

---
+++ server-side/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+//listen
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
