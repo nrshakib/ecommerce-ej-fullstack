14a1aa520c8b23837fa06d30836b68ae3ced23f8
NRS\nrsha <nrshakib007@gmail.com>
2023-08-05 15:18:17 +0600

    updates for fetching data

--- server-side/index.js
+++ server-side/index.js
@@ -37,6 +37,13 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products);
     });
+
+    app.get("/productCount", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const count = await cursor.count();
+      res.send({ count });
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
